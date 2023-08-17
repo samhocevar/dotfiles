8242e5208b2c19320945ee2e19cb269ce23dbd38
Sam Hocevar <sam@hocevar.net>
2023-08-17 15:47:13 +0200

    setup: fix MSYS2 support

--- setup.sh
+++ setup.sh
@@ -3,12 +3,12 @@
 DOTFILES="$(realpath $(dirname $(which $0 2>/dev/null || echo $0)))"
 
 case "$(uname)" in
-    MINGW*)
+    MINGW*|MSYS*)
         case "${MSYS}" in
             *winsymlinks*) ;;
             *) echo "Error: this MSYS2 system does not support symbolic links"
                echo "  uname: $(uname)"
-               echo "  MSYS: ${MSYS}"
+               echo "  \$MSYS: ${MSYS}"
                exit 1 ;;
         esac
 esac
